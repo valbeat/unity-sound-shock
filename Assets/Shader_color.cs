35e5279638b4d9f639368b62b4c1bccb50d139bb
valbeat <kj1ktk@gmail.com>
2015-05-20 20:34:11 +0900

    enable to control shader from script

---
+++ Assets/Shader_color.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Shader_color : MonoBehaviour {
+  public Material mat;
+  public Color color = new Color (1.0f,1.0f,0.0f,1.0f);
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+    	// renderer.material.SetColor("_MyColor", Color (1,1,1,1));
+      mat.SetColor("_MyColor", color);
+		}
+	}
+}
