a73c25e52cbec6372a2b33e0be17d4f5a2c346be
valbeat <kj1ktk@gmail.com>
2015-05-21 17:13:24 +0900

    enable to change emitter color

--- Assets/Shader_color.cs
+++ Assets/Shader_color.cs
@@ -3,15 +3,28 @@ using System.Collections;
 
 public class Shader_color : MonoBehaviour {
   // 色を指定
-  public Color color = new Color (1.0f,1.0f,1.0f,1.0f);
-	Material mat;
+  public Color color1 = new Color (0.0f,0.0f,1.0f,1.0f);
+  public Color color2 = new Color (1.0f,0.0f,0.0f,1.0f);
+
+  Material mat;
   // Use this for initialization
   void Start () {
     mat = GetComponent<Renderer>().material;
-	}
+    StartCoroutine(BlinkerCoroutine());
+  }
 
 	// Update is called once per frame
 	void Update () {
-    mat.SetColor("_MyColor", color);
+  }
+
+  IEnumerator BlinkerCoroutine() {
+    var original_mat = new Material(mat);
+    for(;;){
+      mat.EnableKeyword("_EMISSION");
+      mat.SetColor("_EmissionColor", color1);
+      yield return new WaitForSeconds(1.0f);
+      mat.SetColor("_EmissionColor", color2);
+      yield return new WaitForSeconds(1.0f);
+    }
   }
 }
