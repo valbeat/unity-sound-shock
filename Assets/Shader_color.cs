50b536bab81a8d2fce114454fd70709956085a79
valbeat <kj1ktk@gmail.com>
2015-05-21 15:43:46 +0900

    enable to control the color from Inspector

--- Assets/Shader_color.cs
+++ Assets/Shader_color.cs
@@ -1,20 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-
 public class Shader_color : MonoBehaviour {
-  public Material mat;
-  public Color color = new Color (1.0f,1.0f,0.0f,1.0f);
-	// Use this for initialization
-	void Start () {
+  // 色を指定
+  public Color color = new Color (1.0f,1.0f,1.0f,1.0f);
+	Material mat;
+  // Use this for initialization
+  void Start () {
+    mat = GetComponent<Renderer>().material;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.Space)) {
-    	// renderer.material.SetColor("_MyColor", Color (1,1,1,1));
-      mat.SetColor("_MyColor", color);
-		}
-	}
+    mat.SetColor("_MyColor", color);
+  }
 }
